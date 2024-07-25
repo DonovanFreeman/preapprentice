b63dfe36c2dd73d018884f063fc1affcc0db6049
DonovanFreeman <donovanfreeman0@gmail.com>
2024-07-25 09:26:03 -0400

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,9 @@
+print("Hello World! from Python!\n")
+
+print("I will show you some data!!")
+
+myname = "Donovan Freeman"
+age = "18"
+eyes= "brown"
+print(f"Hello, {myname}. You are {age} years old!")
+print(f"You have {eyes} eyes!!!")
